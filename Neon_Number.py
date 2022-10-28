714e74fbef75d39e9986691fb6f51e8acb8e1750
Gopichand-28 <109718382+Gopichand-28@users.noreply.github.com>
2022-10-28 10:57:07 +0530

    a=int(input()) def neon(num):     b=num*num     y=0     while b>0:         s=b%10         y=s+y         b=b//10     if a==y:         return True     else:         return False if neon(a):     print('Neon Number') else:     print('Not Neon Number')

--- Neon_Number.py
+++ Neon_Number.py
@@ -1,11 +1,16 @@
 a=int(input())
-b=a*a
-y=0
-while b>0:
-    s=b%10
-    y=s+y
-    b=b//10
-if a==y:
+def neon(num):
+    b=num*num
+    y=0
+    while b>0:
+        s=b%10
+        y=s+y
+        b=b//10
+    if a==y:
+        return True
+    else:
+        return False
+if neon(a):
     print('Neon Number')
 else:
     print('Not Neon Number')
